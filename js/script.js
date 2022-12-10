96d9894924f6cf9b27c7021d3ebc05fa2dd36b54
Serena-Dong <serenadong3159@icloud.com>
2022-12-10 19:34:33 +0100

    bonus 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-for (let i = 1; i <= 100; i++ ){
-    if (i % 3 == 0){
-        console.log('Fizz');
+function fizzbuzz() {
+    let display = document.getElementById('display');
+    let displayHTML = "";
+  
+    for (let i = 1; i <= 100; i++ ) { 
+        if (i % 15 == 0){
+        result = 'FizzBuzz';
+        } else if (i % 3 == 0){
+        result = 'Fizz';
+        } else if (i % 5 == 0 ){
+        result = 'Buzz';
+        } else {
+        result = i;
+        }
+        
+      displayHTML += "<p>" + result + "</p>";
     }
-    else if (i % 5 == 0){
-        console.log('Buzz');
-    }
-    else if (i % 15 == 0 ){
-        console.log('FizzBuzz');
-    }
-    else {
-        console.log(i);}
+    display.innerHTML = displayHTML
+  }
 
-}
+
